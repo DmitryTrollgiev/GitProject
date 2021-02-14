56ede1f0f4a1a7fdcc5635aa5ff768e0c8a2081d
Shush1k <shush1k.b@gmail.com>
2021-02-14 13:35:17 +0300

    add root info

--- task1.py
+++ task1.py
@@ -8,7 +8,10 @@ TODO:
 """
 PASSWORD = 1234
 USER_NAME = "Dmitry"
+
+ROOT_PASSWORD = 4321
 def user_info(id):
-    print("User",str(id)+":", USER_NAME, "have pass", PASSWORD)
+    print("\nUser",str(id)+":", USER_NAME, "have pass", PASSWORD)
+    print("Root have pass", ROOT_PASSWORD)
 
 user_info(1)
