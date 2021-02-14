c06f1aab35e3e2f62b68da1bd86b0f318b9f5a99
Shush1k <shush1k.b@gmail.com>
2021-02-14 12:43:36 +0300

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Example Git and GitHub teamwork
+
+TODO:
+    * Some commits
+    * Pull and Pushs
+    * Merge from develop to main
+"""
+PASSWORD = 1234
+USER_NAME = "Dmitry"
+def user_info(id):
+    print("User",str(id)+":", USER_NAME, "have pass", PASSWORD)
+
+user_info(1)
